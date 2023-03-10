be8edba95135bfda1fff50a906b2dd0878f04bb9
Vijay-CPP <vijaytamada02@gmail.com>
2023-03-10 23:32:04 +0530

    70 % completed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,3 +4,9 @@ const hamMenu = document.querySelector("#ham-menu");
 hamBtn.addEventListener('click', () => {
     hamMenu.classList.toggle("hidden");
 })
+
+hamMenu.childNodes.forEach(element => {
+    element.addEventListener("click", () => {
+            hamMenu.classList.toggle("hidden");
+    });
+});
